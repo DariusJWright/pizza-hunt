1738e82db9aaa79a4e1d950f8e5cc343f959fac2
darshnablah <dwright758@gmail.com>
2020-09-17 16:48:56 -0500

    added validation

--- models/pizza.js
+++ models/pizza.js
@@ -1,13 +1,18 @@
 const { Schema, model } = require('mongoose');
 const moment = require('moment');
+const { request } = require('express');
 
 const PizzaSchema = new Schema(
   {
     pizzaName: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -16,6 +21,8 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: 'Large'
     },
     toppings: [],
